07cc907c2bcdc0d75c496943e5b13b977a4f3422
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2017-10-09 18:24:02 +0800

    add simple validation for registration

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const path = require('path');
 
 const app = express();
 const port = process.env.port || 8084;
+// const dbUrl = 'mongodb://eyetuneadmin413:eyetuneadmin413@ds013495.mlab.com:13495/eyetunes';
 const dbUrl = 'mongodb://localhost:27017/eyetunes';
 let db;
 const htmlLink = `${process.cwd()}/public/html`;
 
 app.use("/css", express.static(path.join(process.cwd(), '/public/css')));
-app.use("/resources", express.static(path.join(process.cwd(), '/public/resources')));
-
+// app.use("/resources", express.static(path.join(process.cwd(), '/public/resources')));
+app.use(express.static(path.join(process.cwd(), 'public/html')));
 MongoClient.connect(dbUrl, (error, database) => {
   if (!error) {
-    db = database;
+    db = database;    
     app.listen(port, () => {
       console.log(`App is live @ http://localhost:${port}!`);
     });
@@ -25,5 +26,10 @@ app.get('/admin', (request, response) => {
 });
 
 app.get('/', (request, response) => {
+  db.collection('songs').find().toArray((error, results) => {
+    if (!error) {
+      response.end(JSON.stringify(results));
+    }
+  });
   response.sendFile(`${htmlLink}/index.html`);
 });
